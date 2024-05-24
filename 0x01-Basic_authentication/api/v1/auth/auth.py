07fd511573e1844e206cdd3e44f9c4a16e683e17
READMEmaybe <haroune11411@gmail.com>
2024-05-24 19:52:32 +0100

    fix task 3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,16 +8,16 @@ class Auth:
     """ Auth class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ require_auth
+        """ require_auth method that returns False
         """
         return False
 
     def authorization_header(self, request=None) -> str:
-        """ authorization_header
+        """ authorization_header method that returns None
         """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ current_user
+        """ current_user method that returns None
         """
         return None
