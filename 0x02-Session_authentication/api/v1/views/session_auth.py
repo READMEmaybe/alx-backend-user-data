2a8fa3e1941e0ae1fac83bc2b82829619bdd714a
READMEmaybe <haroune11411@gmail.com>
2024-05-26 01:54:46 +0100

    Add task 8

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -29,3 +29,16 @@ def auth_session_login() -> str:
             response.set_cookie(os.getenv('SESSION_NAME'), session_id)
             return response
     return jsonify({"error": "wrong password"}), 401
+
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
+def auth_session_logout() -> str:
+    """ DELETE /api/v1/auth_session/logout
+    Return:
+      - empty JSON
+    """
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)
