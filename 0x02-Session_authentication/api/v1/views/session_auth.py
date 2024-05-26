88c9351d2837b141a208db1151e2e080be2bb548
READMEmaybe <haroune11411@gmail.com>
2024-05-26 01:51:04 +0100

    Add task 7

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Session Auth flask view """
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+import os
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def auth_session_login() -> str:
+    """ POST /api/v1/auth_session/login
+    Return:
+      - User object JSON represented
+    """
+    email = request.form.get("email")
+    if email is None or email == "":
+        return jsonify({"error": "email missing"}), 400
+    password = request.form.get("password")
+    if password is None or password == "":
+        return jsonify({"error": "password missing"}), 400
+    from models.user import User
+    users = User.search({"email": email})
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            response = jsonify(user.to_json())
+            response.set_cookie(os.getenv('SESSION_NAME'), session_id)
+            return response
+    return jsonify({"error": "wrong password"}), 401
